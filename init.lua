3fca1db5d4f8788264d7d546b5115b910239b190
Kevin <50965376+tgodkev@users.noreply.github.com>
2024-01-23 10:50:22 -0500

    theme changes

--- init.lua
+++ init.lua
@@ -196,17 +196,24 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-      --  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end,
-  },
+  --{
+  -- Theme inspired by Atom
+  --  'navarasu/onedark.nvim',
+  --  priority = 1000,
+  -- config = function()
+  --  vim.cmd.colorscheme 'onedark'
+  --  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  --  end,
+  -- },
 
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    config = function()
+      vim.cmd("colorscheme kanagawa")
+    end
+  },
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
