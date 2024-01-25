478a62686571c854c4ae72bcdbfa3176a6a127bd
Kevin <50965376+tgodkev@users.noreply.github.com>
2024-01-25 00:08:22 -0500

    removed redudant keymaps

--- init.lua
+++ init.lua
@@ -1,43 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -46,9 +6,9 @@ vim.g.maplocalleader = ' '
 
 
 
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-    pattern = "*.postcss",
-    command = "setfiletype css"
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.postcss",
+  command = "setfiletype css"
 })
 
 
@@ -126,7 +86,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
- -- { 'folke/which-key.nvim',  opts = {} },
+  -- { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -341,10 +301,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>l", "$")
-vim.keymap.set("n", "<leader>h", "^")
+
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
@@ -399,9 +356,6 @@ function CreateNewDirectory()
   end
 end
 
-
-
-
 require('telescope').setup {
   defaults = {
     mappings = {
@@ -417,8 +371,8 @@ require('telescope').setup {
 pcall(require('telescope').load_extension, 'fzf')
 
 -- Key mappings for custom functions
-vim.api.nvim_set_keymap('n', '<leader>nf', '<cmd>lua CreateNewFile()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>nd', '<cmd>lua CreateNewDirectory()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>nf', '<cmd>lua CreateNewFile()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>nd', '<cmd>lua CreateNewDirectory()<CR>', { noremap = true, silent = true })
 
 -- Telescope live_grep in git root
 -- Function to find the git root directory based on the current buffer's path
@@ -618,8 +572,8 @@ end
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
 --require('which-key').register({
- -- ['<leader>'] = { name = 'VISUAL <leader>' },
- -- ['<leader>h'] = { 'Git [H]unk' },
+-- ['<leader>'] = { name = 'VISUAL <leader>' },
+-- ['<leader>h'] = { 'Git [H]unk' },
 --}, { mode = 'v' })
 
 -- mason-lspconfig requires that these setup functions are called in this order
