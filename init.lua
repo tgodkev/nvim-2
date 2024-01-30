da6abbcb7b4fe41b85f3dff1e02401a32a3fcf9e
Kevin <50965376+tgodkev@users.noreply.github.com>
2024-01-30 00:25:16 -0500

    cleaned up notify stuff

--- init.lua
+++ init.lua
@@ -726,4 +726,9 @@ cmp.setup {
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 --
-vim.notify = require("notify")
+
+
+-- below is notify stuff
+
+
+
